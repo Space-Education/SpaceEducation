b0cadd68d08b2f6a94482b72686752a53a53c8d5
oussama hmaied <ouss.hmaied.14@gmail.com>
2022-02-10 09:27:47 +0100

    navbar and some css

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,64 @@
 <script>
+import Signup from './components/signup.vue'
 </script>
 
 
 <template>
-  <NavBar />
-  <div>
-    Space Education
-    <router-link to="/Feed"> Feed </router-link>
-    <router-link to="/Post"> Post </router-link>
-    <router-link to="/Signin"> Signin </router-link>
-    <router-link to="/Signup"> Signup </router-link>
-    <router-link to="/AboutUs"> AboutUs </router-link>
-  </div>
-  <router-view/>
+  <div className="center"></div>
+
+  <ul>
+    <li>
+      <a > <router-link to="/Feed"> Feed </router-link> </a>
+    </li>
+    <li>
+      <a> <router-link to="/Post"> Post </router-link> </a>
+    </li>
+    <li>
+      <a> <router-link to="/Signin"> Signin </router-link> </a>
+    </li>
+    <li>
+      <a> <router-link to="/Signup"> Signup </router-link> </a>
+    </li>
+    <li>
+      <a> <router-link to="/AboutUs"> AboutUs </router-link> </a>
+    </li>
+  </ul>
+
+  <router-view />
 </template>
+
+<style>
+/* body {
+  font-size: 28px;
+} */
+.center {
+  text-align: center;
+}
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: rgb(16, 22, 75);
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+  
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 8px 16px;
+  text-decoration: none;
+}
+
+li a:hover {
+  background-color: rgb(67, 33, 146);
+}
+</style>
