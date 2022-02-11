55b70382b06bfc35bf69202349966b65248a3780
oussama hmaied <ouss.hmaied.14@gmail.com>
2022-02-11 16:12:17 +0100

    more front end fns

--- src/components/PostLecture.vue
+++ src/components/PostLecture.vue
@@ -1,17 +1,24 @@
 
 <script>
-import App from "../App.vue";
 import axios from "axios";
 export default {
+  components: {},
   data() {
     return {
       title: "",
       description: "",
-      datatype: JSON.parse(localStorage.getItem("type")),
+      datatype: [],
       user: JSON.parse(localStorage.getItem("user")),
       id_type: null,
       // url: 'http://localhost:3001/api/items/getdata'
     };
+  },
+  mounted() {
+    axios.get("http://localhost:3001/api/items/type").then((response) => {
+      this.datatype = response.data;
+      console.log(this.datatype);
+      // localStorage.setItem("type", JSON.stringify(this.type));
+    });
   },
   methods: {
     change(event) {
@@ -19,12 +26,12 @@ export default {
       console.log(this[event.target.name]);
     },
     add() {
-      var option = { 
+      var option = {
         title: this.title,
         description: this.description,
-        image_post:this.image_post,
+        image_post: this.image_post,
         id_type: this.id_type,
-        id_user:this.user.id_user
+        id_user: this.user.id_user,
       };
       console.log(option);
       axios
@@ -42,130 +49,74 @@ export default {
 
 
 <template>
-  <!-- <div>
-        
-  </div>-->
-  <div class="login-root">
-    <div
-      class="box-root flex-flex flex-direction--column"
-      style="min-height: 100vh; flex-grow: 1"
-    >
-      <div class="loginbackground box-background--white padding-top--64">
-        <div class="loginbackground-gridContainer">
-          <div
-            class="box-root flex-flex"
-            style="grid-area: top / start / 8 / end"
-          >
-            <div
-              class="box-root"
-              style="
-                background-image: linear-gradient(
-                  white 0%,
-                  rgb(247, 250, 252) 33%
-                );
-                flex-grow: 1;
-              "
-            ></div>
+<br>
+  <div class="formbg">
+    <div class="formbg-inner padding-horizontal--48">
+      <h3>Post a lecture:</h3>
+      <div class="row">
+        <div class="col">
+          <!-- Name input -->
+          <div class="form-outline">
+            <label class="form-label" for="form8Example1">Title:</label>
+            <input type="text" id="form8Example1" class="form-control" />
           </div>
-          <div class="box-root flex-flex" style="grid-area: 4 / 2 / auto / 5">
-            <div
-              class="
-                box-root
-                box-divider--light-all-2
-                animationLeftRight
-                tans3s
-              "
-              style="flex-grow: 1"
-            ></div>
-          </div>
-          <div
-            class="box-root flex-flex"
-            style="grid-area: 6 / start / auto / 2"
-          >
-            <div
-              class="box-root box-background--blue800"
-              style="flex-grow: 1"
-            ></div>
-          </div>
-          <div
-            class="box-root flex-flex"
-            style="grid-area: 7 / start / auto / 4"
-          >
-            <div
-              class="box-root box-background--blue animationLeftRight"
-              style="flex-grow: 1"
-            ></div>
-          </div>
-          <div class="box-root flex-flex" style="grid-area: 8 / 4 / auto / 6">
-            <div
-              class="box-root box-background--gray100 animationLeftRight tans3s"
-              style="flex-grow: 1"
-            ></div>
-          </div>
-          <div
-            class="box-root flex-flex"
-            style="grid-area: 2 / 15 / auto / end"
-          >
-            <div
-              class="box-root box-background--cyan200 animationRightLeft tans4s"
-              style="flex-grow: 1"
-            ></div>
-          </div>
-          <div
-            class="box-root flex-flex"
-            style="grid-area: 3 / 14 / auto / end"
-          >
-            <div
-              class="box-root box-background--blue animationRightLeft"
-              style="flex-grow: 1"
-            ></div>
-          </div>
-          <div class="box-root flex-flex" style="grid-area: 4 / 17 / auto / 20">
-            <div
-              class="box-root box-background--gray100 animationRightLeft tans4s"
-              style="flex-grow: 1"
-            ></div>
-          </div>
-          <div class="box-root flex-flex" style="grid-area: 5 / 14 / auto / 17">
-            <div
-              class="
-                box-root
-                box-divider--light-all-2
-                animationRightLeft
-                tans3s
-              "
-              style="flex-grow: 1"
-            ></div>
+        </div>
+        <div class="col">
+          <!-- Email input -->
+          <div class="form-outline">
+            <label class="form-label" for="form8Example2">Description:</label>
+            <input type="email" id="form8Example2" class="form-control" />
           </div>
         </div>
       </div>
-      <div
-        class="box-root padding-top--24 flex-flex flex-direction--column"
-        style="flex-grow: 1; z-index: 9"
-      >
-        <div
-          class="
-            box-root
-            padding-top--48
-            padding-bottom--24
-            flex-flex
-            flex-justifyContent--center
-          "
-        >
-          <h1></h1>
+
+      <hr />
+
+      <div class="row">
+        <div class="col">
+          <!-- Name input -->
+          <div class="form-outline">
+            <label class="form-label" for="form8Example2"
+              >select the categorie:</label
+            >
+
+            <select class="form-select" aria-label="Default select example" name="id_type" @input="change">
+              <option value="">select type of lecture</option>
+
+              <option
+                v-for="elem in this.datatype"
+                :key="elem"
+                :value="elem.id_type"
+              >
+                {{ elem.label_type }}
+              </option>
+            </select>
+          </div>
         </div>
-        <div class="formbg-outer">
-          <div class="formbg">
-            <div class="formbg-inner padding-horizontal--48">
-              <span class="padding-bottom--15">Add a lecture</span>
-              <!-- <form id="stripe-login"> -->
+        <div class="col">
+          <!-- Name input -->
+          <div class="form-outline">
+            <label class="form-label" for="form8Example4">Image:</label>
+            <input type="text" id="form8Example4" class="form-control" />
+          </div>
+        </div>
+        <div class="col">
+          <!-- Email input -->
+          <div class="form-outline">
+                    <label class="form-label" for="form8Example4">Click to submit:</label>
+    
+                <button class="btn btn-primary btn-block mb-4" v-on:click="add()">Submit</button>
+          </div>
+        </div>
+      </div>
+      <!-- <span class="padding-bottom--15">Add a lecture</span>
               <div class="field padding-bottom--24">
-                <h1>Title</h1>
+                <label>Title</label>
                 <input name="title" :value="title" @input="change" />
                 
-                <h1>description</h1>
+                <label>description</label>
                 <input name="description" @input="change2" />
-                <h1>type of The Lecture</h1>
+                <label>type of The Lecture</label>
                 <select name="id_type" @input="change">
                   <option value="">select type of lecture</option>
 
@@ -178,14 +129,12 @@ export default {
                   </option>
                 </select>
 
-                <h1>image</h1>
+                <label>image</label>
                 <input name="image_post" type="text" @input="change"/>
-                <button v-on:click="add()">Submit</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+                <br>
+                <br>
+                <button class="btn btn-primary btn-block mb-4" v-on:click="add()">Submit</button>
+              </div> -->
     </div>
   </div>
 </template>
@@ -249,9 +198,9 @@ a {
 .box-background--blue {
   background-color: #5469d4;
 }
-.box-background--white {
+/* .box-background--white {
   background-color: #ffffff;
-}
+} */
 .box-background--blue800 {
   background-color: #212d63;
 }
@@ -288,7 +237,7 @@ a {
 .formbg {
   margin: 0px auto;
   width: 100%;
-  max-width: 448px;
+  max-width: 800px;
   background: white;
   border-radius: 4px;
   box-shadow: rgba(60, 66, 87, 0.12) 0px 7px 14px 0px,
